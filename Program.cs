ac119187f597e7b166c633f00cf3670cbc910b08
456477617264 <ec557782@gmail.com>
2023-11-03 17:32:42 +0000

    GraphQL Server

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using HotChocolate;
+using OakGraphQLServer.Queries;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
+var app = builder.Build();
+
+app.MapGraphQL();
+
+app.Run();
---
+++ Queries/Query.cs
@@ -0,0 +1,8 @@
+namespace OakGraphQLServer.Queries
+{
+    public class Query
+    {  
+        // Demo hello world query
+        public string Hello() => "Hello World!";
+    }
+}
